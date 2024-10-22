a8cee67d5b42d79707a1b07327a6ffc2d6afd593
Arjun-kris <arjunkrishnar00@gmail.com>
2024-10-23 04:54:55 +0530

    first commit

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Experience.module.css";
+import history from "../../data/history.json";
+
+export const Experience = () => {
+  return (
+    <section className={styles.container} id="experience">
+      <h2 className={styles.title}>Experience</h2>
+      <div>
+        <ul className={styles.history}>
+          {history.map((historyItem, id) => {
+            return (
+              <li key={id} className={styles.historyItem}>
+                <div className={styles.historyItemDetails}>
+                  <h3>{`${historyItem.role} ,`}</h3>
+                  <h4>{historyItem.organisation}</h4>
+                  <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                  <ul>
+                    {historyItem.experiences.map((experience, id) => {
+                      return <li key={id}>{experience}</li>;
+                    })}
+                  </ul>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
+};
